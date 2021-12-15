597c389484b26d2783d86fd350e1e80ab40d99c5
Lora Horst <lora.horst@t-online.de>
2021-12-15 19:29:48 +0100

    Step 1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+const canvas = document.querySelector(".canvas")
+const ctx = canvas.getContext("2d")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('keydown', (e) => {
+    switch (e.keyCode) {
+      case 87: // w
+        player.speedY -= 1;
+        break;
+      case 83: // s
+        player.speedY += 1;
+        break;
+      case 65: // a
+        player.speedX -= 1;
+        break;
+      case 68: // d
+        player.speedX += 1;
+        break;
+    }
+  });
