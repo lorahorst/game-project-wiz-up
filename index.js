7b8cb901c6f187d766d73f45fa0a24ed2f2b37e9
Lora Horst <lora.horst@t-online.de>
2021-12-18 14:23:45 +0100

    Step 18 'Little Code Clean-Up'

--- index.js
+++ index.js
@@ -20,18 +20,13 @@ wallImage.src = "./images/wall.jpg"
 // Classes
 
 class Obstacle {
-    constructor(argW, argH, argColor, argX, argY, argImg) {
+    constructor(argW, argH, argX, argY, argImg) {
         this.w = argW;
         this.h = argH;
-        this.color = argColor;
         this.x = argX;
         this.y = argY;
         this.img = argImg
     }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x,this.y,this.w, this.h)
-   }
    paint () {
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
 }
@@ -39,10 +34,9 @@ class Obstacle {
 
 
 class Player {
-    constructor(argW, argH, argColor, argX, argY, argHealth, argStrength, argID, argImg) {
+    constructor(argW, argH, argX, argY, argHealth, argStrength, argID, argImg) {
       this.w = argW;
       this.h = argH;
-      this.color = argColor;
       this.x = argX;
       this.y = argY;
       this.health = argHealth;
@@ -50,10 +44,6 @@ class Player {
       this.id = argID;
       this.img = argImg
     }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x,this.y,this.w, this.h)
-       }   
     paint () {
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
     }
@@ -186,20 +176,15 @@ class Player {
 }
 
     class Item {
-        constructor(argW, argH, argColor, argX, argY, argID, argEffect, argImg) {
+        constructor(argW, argH, argX, argY, argID, argEffect, argImg) {
             this.w = argW;
             this.h = argH;
-            this.color = argColor;
             this.x = argX;
             this.y = argY;
             this.id = argID;
             this.effect = argEffect;
             this.img = argImg
         }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x,this.y,this.w, this.h)
-       }
     paint () {
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
         }
@@ -209,82 +194,82 @@ class Player {
 
 //Variables
 
-    const wizard1 = new Player (35, 35, 'green', 260, 410, 10, 3, 0, playerImage);
+    const wizard1 = new Player (35, 35, 260, 410, 10, 3, 0, playerImage);
 
     let monsters = [
-        new Player (30, 30, 'red', 55, 55, 5, 2, '1', monImage),
-        new Player (30, 30, 'red', 180, 410, 5, 2, '2', monImage)
+        new Player (30, 30, 55, 55, 5, 2, '1', monImage),
+        new Player (30, 30, 180, 410, 5, 2, '2', monImage)
         ]
 
     const walls = [
 //Exterior Walls
-        new Obstacle (50, 50, 'grey', 0, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 50, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 100, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 150, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 200, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 250, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 300, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 350, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 400, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 0, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 50, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 100, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 150, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 200, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 250, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 300, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 350, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 400, wallImage),
-        new Obstacle (50, 50, 'grey', 0, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 50, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 100, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 150, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 200, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 250, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 300, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 350, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 400, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 450, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 50, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 100, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 150, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 200, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 250, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 300, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 350, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 400, wallImage),
-        new Obstacle (50, 50, 'grey', 450, 450, wallImage),
+        new Obstacle (50, 50, 0, 0, wallImage),
+        new Obstacle (50, 50, 50, 0, wallImage),
+        new Obstacle (50, 50, 100, 0, wallImage),
+        new Obstacle (50, 50, 150, 0, wallImage),
+        new Obstacle (50, 50, 200, 0, wallImage),
+        new Obstacle (50, 50, 250, 0, wallImage),
+        new Obstacle (50, 50, 300, 0, wallImage),
+        new Obstacle (50, 50, 350, 0, wallImage),
+        new Obstacle (50, 50, 400, 0, wallImage),
+        new Obstacle (50, 50, 450, 0, wallImage),
+        new Obstacle (50, 50, 0, 50, wallImage),
+        new Obstacle (50, 50, 0, 100, wallImage),
+        new Obstacle (50, 50, 0, 150, wallImage),
+        new Obstacle (50, 50, 0, 200, wallImage),
+        new Obstacle (50, 50, 0, 250, wallImage),
+        new Obstacle (50, 50, 0, 300, wallImage),
+        new Obstacle (50, 50, 0, 350, wallImage),
+        new Obstacle (50, 50, 0, 400, wallImage),
+        new Obstacle (50, 50, 0, 450, wallImage),
+        new Obstacle (50, 50, 50, 450, wallImage),
+        new Obstacle (50, 50, 100, 450, wallImage),
+        new Obstacle (50, 50, 150, 450, wallImage),
+        new Obstacle (50, 50, 200, 450, wallImage),
+        new Obstacle (50, 50, 250, 450, wallImage),
+        new Obstacle (50, 50, 300, 450, wallImage),
+        new Obstacle (50, 50, 350, 450, wallImage),
+        new Obstacle (50, 50, 400, 450, wallImage),
+        new Obstacle (50, 50, 450, 450, wallImage),
+        new Obstacle (50, 50, 450, 50, wallImage),
+        new Obstacle (50, 50, 450, 100, wallImage),
+        new Obstacle (50, 50, 450, 150, wallImage),
+        new Obstacle (50, 50, 450, 200, wallImage),
+        new Obstacle (50, 50, 450, 250, wallImage),
+        new Obstacle (50, 50, 450, 300, wallImage),
+        new Obstacle (50, 50, 450, 350, wallImage),
+        new Obstacle (50, 50, 450, 400, wallImage),
+        new Obstacle (50, 50, 450, 450, wallImage),
 //Interior Walls
-new Obstacle (50, 50, 'grey', 50, 300, wallImage),
-new Obstacle (50, 50, 'grey', 50, 350, wallImage),
-new Obstacle (50, 50, 'grey', 50, 400, wallImage),
-new Obstacle (50, 50, 'grey', 50, 450, wallImage),
-new Obstacle (50, 50, 'grey', 400, 400, wallImage),
-new Obstacle (50, 50, 'grey', 400, 200, wallImage),
-new Obstacle (50, 50, 'grey', 400, 100, wallImage),
-new Obstacle (50, 50, 'grey', 400, 50, wallImage),
-new Obstacle (50, 50, 'grey', 350, 50, wallImage),
-new Obstacle (50, 50, 'grey', 250, 50, wallImage),
-new Obstacle (50, 50, 'grey', 200, 250, wallImage),
-new Obstacle (50, 50, 'grey', 200, 200, wallImage),
-new Obstacle (50, 50, 'grey', 200, 300, wallImage),
-new Obstacle (50, 50, 'grey', 200, 150, wallImage),
-new Obstacle (50, 50, 'grey', 200, 350, wallImage),
-new Obstacle (50, 50, 'grey', 150, 350, wallImage),
-new Obstacle (50, 50, 'grey', 150, 250, wallImage),
-new Obstacle (50, 50, 'grey', 300, 350, wallImage),
-new Obstacle (50, 50, 'grey', 300, 300, wallImage),
-new Obstacle (50, 50, 'grey', 350, 300, wallImage),
-new Obstacle (50, 50, 'grey', 300, 200, wallImage),
-new Obstacle (50, 50, 'grey', 300, 150, wallImage),
-new Obstacle (50, 50, 'grey', 100, 100, wallImage),
-new Obstacle (50, 50, 'grey', 100, 150, wallImage),
+new Obstacle (50, 50, 50, 300, wallImage),
+new Obstacle (50, 50, 50, 350, wallImage),
+new Obstacle (50, 50, 50, 400, wallImage),
+new Obstacle (50, 50, 50, 450, wallImage),
+new Obstacle (50, 50, 400, 400, wallImage),
+new Obstacle (50, 50, 400, 200, wallImage),
+new Obstacle (50, 50, 400, 100, wallImage),
+new Obstacle (50, 50, 400, 50, wallImage),
+new Obstacle (50, 50, 350, 50, wallImage),
+new Obstacle (50, 50, 250, 50, wallImage),
+new Obstacle (50, 50, 200, 250, wallImage),
+new Obstacle (50, 50, 200, 200, wallImage),
+new Obstacle (50, 50, 200, 300, wallImage),
+new Obstacle (50, 50, 200, 150, wallImage),
+new Obstacle (50, 50, 200, 350, wallImage),
+new Obstacle (50, 50, 150, 350, wallImage),
+new Obstacle (50, 50, 150, 250, wallImage),
+new Obstacle (50, 50, 300, 350, wallImage),
+new Obstacle (50, 50, 300, 300, wallImage),
+new Obstacle (50, 50, 350, 300, wallImage),
+new Obstacle (50, 50, 300, 200, wallImage),
+new Obstacle (50, 50, 300, 150, wallImage),
+new Obstacle (50, 50, 100, 100, wallImage),
+new Obstacle (50, 50, 100, 150, wallImage),
        ];
     
     let potions = [
-           new Item (25, 25, 'blue', 410, 165, '1', 2, itemImage),
-           new Item (25, 25, 'blue', 410, 260, '2', 3, itemImage)
+           new Item (25, 25, 410, 165, '1', 2, itemImage),
+           new Item (25, 25, 410, 260, '2', 3, itemImage)
         ]
 
 
